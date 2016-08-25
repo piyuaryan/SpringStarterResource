43f5d536895ef9fd0cf1e4b34f299fbed98fbccf
piyu.aryan@gmail.com <piyu.aryan@gmail.com>
2016-08-26 00:02:19 +1000

    Added sample controller and port

--- src/main/java/com/example/ResourceApplication.java
+++ src/main/java/com/example/ResourceApplication.java
@@ -2,11 +2,42 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
 
 @SpringBootApplication
+@RestController
 public class ResourceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ResourceApplication.class, args);
-	}
+    @RequestMapping("/")
+    public Message home() {
+        return new Message("Hello World");
+    }
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(ResourceApplication.class, args);
+    }
+}
+
+class Message {
+    private String id = UUID.randomUUID().toString();
+    private String content;
+
+    Message() {
+    }
+
+    public Message(String content) {
+        this.content = content;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
 }
